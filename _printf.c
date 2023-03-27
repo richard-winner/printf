e6b7c82fd016e7edcff89baf92cd2957de5b076d
dr <duvorrichardwinner@gmail.com>
2023-03-27 06:52:43 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
+
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- */
+* _printf - Printf function
+* @format: format.
+* Return: Printed chars.
+*/
 
 int _printf(const char *format, ...)
-
 {
+
 int i, printed = 0, printed_chars = 0;
 int flags, width, precision, size, buff_ind = 0;
 va_list list;
@@ -50,12 +51,10 @@ return (printed_chars);
 }
 
 /**
- * print_buffer - Prints the contents of the
- * buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char,
- * represents the length.
- */
+* print_buffer - Prints the contents of the buffer if it exist
+* @buffer: Array of chars
+* @buff_ind: Index at which to add next char, represents the length.
+*/
 
 void print_buffer(char buffer[], int *buff_ind)
 {
